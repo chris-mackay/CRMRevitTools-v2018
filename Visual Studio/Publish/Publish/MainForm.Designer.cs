af13d77bfbc4310fa3b40a42079e07c5284734ad
Christopher Ryan Mackay <cmackay732@gmail.com>
2020-09-25 21:50:21 -0400

    Publish now prints and renames files.

---
+++ Visual Studio/Publish/Publish/MainForm.Designer.cs
@@ -0,0 +1,210 @@
+﻿//    Copyright(C) 2020  Christopher Ryan Mackay
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program.If not, see<https://www.gnu.org/licenses/>.
+
+namespace Publish
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbRevisions = new System.Windows.Forms.ComboBox();
+            this.lblRevision = new System.Windows.Forms.Label();
+            this.btnPublish = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbSequence = new System.Windows.Forms.RadioButton();
+            this.rbNumber = new System.Windows.Forms.RadioButton();
+            this.rbDate = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbRevisions
+            // 
+            this.cbRevisions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbRevisions.FormattingEnabled = true;
+            this.cbRevisions.Location = new System.Drawing.Point(12, 209);
+            this.cbRevisions.Name = "cbRevisions";
+            this.cbRevisions.Size = new System.Drawing.Size(415, 21);
+            this.cbRevisions.TabIndex = 3;
+            this.cbRevisions.TabStop = false;
+            this.cbRevisions.SelectedIndexChanged += new System.EventHandler(this.cbRevisions_SelectedIndexChanged);
+            // 
+            // lblRevision
+            // 
+            this.lblRevision.AutoSize = true;
+            this.lblRevision.Location = new System.Drawing.Point(12, 187);
+            this.lblRevision.Name = "lblRevision";
+            this.lblRevision.Size = new System.Drawing.Size(90, 13);
+            this.lblRevision.TabIndex = 2;
+            this.lblRevision.Text = "Revision Property";
+            // 
+            // btnPublish
+            // 
+            this.btnPublish.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnPublish.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnPublish.Location = new System.Drawing.Point(271, 269);
+            this.btnPublish.Name = "btnPublish";
+            this.btnPublish.Size = new System.Drawing.Size(75, 23);
+            this.btnPublish.TabIndex = 4;
+            this.btnPublish.TabStop = false;
+            this.btnPublish.Text = "Publish";
+            this.btnPublish.UseVisualStyleBackColor = true;
+            this.btnPublish.Click += new System.EventHandler(this.btnPublish_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(352, 269);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.TabStop = false;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a revision from the dropdown list below to create\r\na sheet set containing " +
+    "all the sheets with the selected \r\nrevision property";
+            // 
+            // rbSequence
+            // 
+            this.rbSequence.AutoSize = true;
+            this.rbSequence.Location = new System.Drawing.Point(22, 23);
+            this.rbSequence.Name = "rbSequence";
+            this.rbSequence.Size = new System.Drawing.Size(118, 17);
+            this.rbSequence.TabIndex = 0;
+            this.rbSequence.Text = "Revision Sequence";
+            this.rbSequence.UseVisualStyleBackColor = true;
+            this.rbSequence.CheckedChanged += new System.EventHandler(this.radioButtonCheckChanged);
+            // 
+            // rbNumber
+            // 
+            this.rbNumber.AutoSize = true;
+            this.rbNumber.Location = new System.Drawing.Point(22, 46);
+            this.rbNumber.Name = "rbNumber";
+            this.rbNumber.Size = new System.Drawing.Size(106, 17);
+            this.rbNumber.TabIndex = 1;
+            this.rbNumber.Text = "Revision Number";
+            this.rbNumber.UseVisualStyleBackColor = true;
+            this.rbNumber.CheckedChanged += new System.EventHandler(this.radioButtonCheckChanged);
+            // 
+            // rbDate
+            // 
+            this.rbDate.AutoSize = true;
+            this.rbDate.Location = new System.Drawing.Point(22, 69);
+            this.rbDate.Name = "rbDate";
+            this.rbDate.Size = new System.Drawing.Size(92, 17);
+            this.rbDate.TabIndex = 2;
+            this.rbDate.Text = "Revision Date";
+            this.rbDate.UseVisualStyleBackColor = true;
+            this.rbDate.CheckedChanged += new System.EventHandler(this.radioButtonCheckChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbNumber);
+            this.groupBox1.Controls.Add(this.rbDate);
+            this.groupBox1.Controls.Add(this.rbSequence);
+            this.groupBox1.Location = new System.Drawing.Point(15, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(161, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Revision Properties";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.cbRevisions);
+            this.panel1.Controls.Add(this.lblRevision);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(439, 255);
+            this.panel1.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 304);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnPublish);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Publish";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbRevisions;
+        private System.Windows.Forms.Label lblRevision;
+        private System.Windows.Forms.Button btnPublish;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbSequence;
+        private System.Windows.Forms.RadioButton rbNumber;
+        private System.Windows.Forms.RadioButton rbDate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ Visual Studio/Publish/Publish/MainForm.cs
@@ -0,0 +1,326 @@
+﻿//    Copyright(C) 2020  Christopher Ryan Mackay
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program.If not, see<https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Publish
+{
+    public partial class MainForm : System.Windows.Forms.Form
+    {
+        #region CLASS_LEVEL_VARIABLES
+
+        UIApplication myRevitUIApp = null;
+        Document myRevitDoc = null;
+
+        public IList<Element> viewSheets = null;
+        public string REVIT_VERSION = "v2018";
+
+        ViewSet set = null;
+        FileSystemWatcher w = null;
+
+        #endregion
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public MainForm(UIApplication incomingUIApp)
+        {
+            InitializeComponent();
+
+            myRevitUIApp = incomingUIApp;
+            myRevitDoc = myRevitUIApp.ActiveUIDocument.Document;
+            
+
+            FilteredElementCollector sheetsCol = new FilteredElementCollector(myRevitDoc);
+            viewSheets = sheetsCol.OfClass(typeof(ViewSheet)).ToElements();
+
+            rbSequence.Checked = true;
+            btnPublish.Enabled = false;
+
+            LoadItems();
+        }
+
+        private void OnCreated(object source, FileSystemEventArgs eventArgs)
+        {
+            // Check the count and names of files in the directory.
+            // Confirm that all the correct files are finished printing
+            // If all files have printed trigger renaming of files.
+
+            DirectoryInfo dInfo = new DirectoryInfo(@"C:\Users\cmackay\Desktop\Print");
+            int fCount = Directory.GetFiles(dInfo.FullName, "*", SearchOption.TopDirectoryOnly).Length;
+
+            // Checks to make sure the number of file printed equals the number in the sheet set.
+            // If the counts match then rename all the files.
+            if (fCount == set.Size)
+            {
+                #region RenameFiles
+
+                string[] fileEntries = Directory.GetFiles(dInfo.FullName);
+
+                List<string> oldFilesInDirectory = new List<string>();
+
+                foreach (string oldFile in fileEntries)
+                    oldFilesInDirectory.Add(oldFile);
+
+                IList<Element> viewSheetSets = null;
+                ViewSet viewSet = null;
+                List<string> newFiles = new List<string>();
+
+                FilteredElementCollector sheetSetsCol = new FilteredElementCollector(myRevitDoc);
+                viewSheetSets = sheetSetsCol.OfClass(typeof(ViewSheetSet)).ToElements();
+
+                //GET ALL THE SHEETS FROM THE SHEETSET SELECTED
+                foreach (ViewSheetSet vs in viewSheetSets)
+                    if (vs.Name == cbRevisions.SelectedItem.ToString())
+                        viewSet = vs.Views;
+
+                List<string> reOrderedFiles = new List<string>();
+
+                foreach (ViewSheet oldSheet in viewSet)
+                {
+                    string sheetNumber = string.Empty;
+                    string sheetName = string.Empty;
+
+                    sheetNumber = oldSheet.SheetNumber;
+                    sheetName = oldSheet.Name;
+
+                    // SHEET NUMBER NEEDS TO BE CHECKED FOR THE FOLLOWING SPECIAL CHARACTERS BELOW
+
+                    // THESE NEED TO BE REPLACED WITH '-'
+                    // / * " .
+
+                    // REVIT CHECKS FOR THE FOLLOWING CHARACTERS BELOW AND DON'T NEED TO BE HANDLED
+                    // \ : {} [] ; < > ? ` ~
+
+                    // REVIT & WINDOWS ALLOW THE CHARACTERS BELOW
+                    // ! @ # $ % ^ & * ( ) _ + = - ' ,
+
+                    if (sheetNumber.Contains(@"/")) sheetNumber = sheetNumber.Replace(@"/", "-");
+
+                    if (sheetNumber.Contains("*")) sheetNumber = sheetNumber.Replace("*", "-");
+
+                    if (sheetNumber.Contains("\"")) sheetNumber = sheetNumber.Replace("\"", "-");
+
+                    if (sheetNumber.Contains(".")) sheetNumber = sheetNumber.Replace(".", "-");
+
+                    string rev = string.Empty;
+
+                    rev = oldSheet.LookupParameter("Current Revision").AsString();
+
+                    string newFileName = string.Empty;
+                    string newFile = string.Empty;
+
+                    string projectNumber = string.Empty;
+                    projectNumber = myRevitDoc.ProjectInformation.LookupParameter("Project Number").AsString();
+
+                    newFileName = projectNumber + "-" + sheetNumber + "_" + rev + ".pdf";
+                    newFile = dInfo.FullName + "\\" + newFileName;
+                    newFiles.Add(newFile);
+
+                    foreach (string file in oldFilesInDirectory)
+                        if (file.Contains(sheetNumber))
+                            reOrderedFiles.Add(file);
+                }
+
+                int index = 0;
+
+                //LOOP THROUGH EACH FILE IN THE DIRECTORY AND RENAME THE FILE
+                foreach (string oldFile in reOrderedFiles)
+                {
+                    string newF = string.Empty;
+                    newF = newFiles[index];
+
+                    if (File.Exists(newF))
+                        File.Delete(newF);
+
+                    File.Move(oldFile, newF);
+
+                    index += 1;
+                }
+
+                w.Dispose();
+
+                TaskDialog dialog = new TaskDialog("Publish");
+                dialog.MainInstruction = "Published successfully";
+                dialog.Show();
+
+                #endregion
+            }
+
+        }
+
+        private void Publish()
+        {
+            string prop = cbRevisions.SelectedItem.ToString();
+            int selectedSequence = RevisionSequenceNumber(prop);
+            Transaction trans = new Transaction(myRevitDoc, "Publish");
+
+            try
+            {
+                #region CreateSheetSet
+
+                set = new ViewSet();
+
+                foreach (ViewSheet vss in viewSheets)
+                {
+                    IList<ElementId> revisionIds = vss.GetAllRevisionIds();
+
+                    foreach (ElementId i in revisionIds)
+                    {
+                        Element elem = myRevitDoc.GetElement(i);
+                        Revision r = elem as Revision;
+
+                        int sequenceNumber = r.SequenceNumber;
+                        string num = vss.GetRevisionNumberOnSheet(i);
+                        string date = r.RevisionDate;
+
+                        if (rbSequence.Checked)
+                            if (selectedSequence == sequenceNumber)
+                                set.Insert(vss);
+                            else if (rbNumber.Checked)
+                                if (num == prop)
+                                    set.Insert(vss);
+                                else
+                                if (date == prop)
+                                    set.Insert(vss);
+                    }
+                }
+
+                PrintManager print = myRevitDoc.PrintManager;
+                print.PrintRange = PrintRange.Select;
+                ViewSheetSetting viewSheetSetting = print.ViewSheetSetting;
+                viewSheetSetting.CurrentViewSheetSet.Views = set;
+                    
+                trans.Start();
+                viewSheetSetting.SaveAs(prop);
+                trans.Commit();
+
+                #endregion
+         
+                string dir = @"C:\Users\cmackay\Desktop\Print";
+
+                // Create a file listener and wait until all the files have been printed before renaming
+                w = new FileSystemWatcher();
+                w.Path = dir;
+                w.NotifyFilter = NotifyFilters.LastAccess
+                                | NotifyFilters.LastWrite
+                                | NotifyFilters.FileName
+                                | NotifyFilters.DirectoryName;
+
+                w.Filter = "*.pdf";
+                w.Created += OnCreated;
+
+                w.EnableRaisingEvents = true;
+
+                // Print the newly created sheet set
+                print.SubmitPrint();
+                print.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+                TaskDialog error = new TaskDialog("Publish");
+                error.MainInstruction = "Failed to publish " + prop;
+                error.MainContent = ex.Message;
+                trans.RollBack();
+                error.Show();
+            }
+        }
+
+        private void btnPublish_Click(object sender, EventArgs e)
+        {
+            Publish();
+        }
+
+        private void cbRevisions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbRevisions.SelectedIndex == -1) 
+                btnPublish.Enabled = false;
+            else 
+                btnPublish.Enabled = true;
+        }
+
+        private int RevisionSequenceNumber(string selectedSequenceName)
+        {
+            int seqNum = 0;
+
+            int from = selectedSequenceName.IndexOf("Seq. ") + "Seq. ".Length;
+            int to = selectedSequenceName.IndexOf(" - ");
+
+            string num = selectedSequenceName.Substring(from, to - from);
+            num = num.Trim();
+
+            seqNum = int.Parse(num);
+
+            return seqNum;
+        }
+
+        private string RevisionSequenceName(Revision revision, string desc)
+        {
+            string seqName = string.Empty;
+
+            seqName = "Seq. " + revision.SequenceNumber + " - " + desc;
+
+            return seqName;
+        }
+
+        private void LoadItems()
+        {
+            foreach (ViewSheet vss in viewSheets)
+            {
+                IList<ElementId> revisionIds = vss.GetAllRevisionIds();
+
+                foreach (ElementId i in revisionIds)
+                {
+                    Element elem = myRevitDoc.GetElement(i);
+                    Revision r = elem as Revision;
+
+                    if (rbSequence.Checked)
+                    {
+                        string sequenceName = RevisionSequenceName(r, r.Description);
+
+                        if (!cbRevisions.Items.Contains(sequenceName))
+                            cbRevisions.Items.Add(sequenceName);
+                    }
+                    else if (rbNumber.Checked)
+                        if (!cbRevisions.Items.Contains(vss.GetRevisionNumberOnSheet(i)))
+                            cbRevisions.Items.Add(vss.GetRevisionNumberOnSheet(i));
+                    else
+                        if (!cbRevisions.Items.Contains(r.RevisionDate))
+                            cbRevisions.Items.Add(r.RevisionDate);
+                }
+            }
+        }
+
+        private void radioButtonCheckChanged(object sender, EventArgs e)
+        {
+            cbRevisions.Items.Clear();
+            cbRevisions.SelectedIndex = -1;
+
+            if (cbRevisions.SelectedIndex == -1)
+                btnPublish.Enabled = false;
+            else
+                btnPublish.Enabled = true;
+
+            LoadItems();
+        }
+    }
+}
